a48645dd1078191a33aa46f6ff1b16aec56b2beb
ub <junhyeog@korea.ac.kr>
2021-06-10 18:19:12 +0900

    feat: first work

---
+++ select-client/select-client.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+/** Constants
+ * BUF_SIZE : TCP payload size
+*/
+#define BUF_SIZE 1 << 16
+
+/** Global variables
+ * char user_mes[BUF_SIZE]: buffer for user message
+ * char echoed_mes[BUF_SIZE] : buffer for echoed message
+*/
+char user_mes[BUF_SIZE];
+char echoed_mes[BUF_SIZE];
+
+void handle_error(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(EXIT_FAILURE);
+}
+
+void usage()
+{
+    printf("syntax : ./select-client <ip address> <port number>\n");
+    printf("sample : ./select-client 112.113.114.115 8888\n");
+    handle_error("[-] Usage error");
+}
+
+int main(int argc, char *argv[])
+{
+    // Result value
+    int res;
+
+    // Check command-line arguments
+    if (argc != 3)
+        usage();
+
+    // Parse command-line arguments
+    int port_num = atoi(argv[2]);
+    struct in_addr ip;
+    res = inet_pton(AF_INET, argv[1], &ip);
+    if (res < 0)
+        handle_error("[-] Fail to get a IP address");
+    else if (!res)
+        handle_error("[-] Invalid IP address");
+    if (port_num < 0 || (int)(1 << 16) <= port_num)
+        handle_error("[-] Invalid port number");
+
+    // Create a socket
+    // IPv4 Internet protocols and TCP
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        handle_error("[-] Failed to create a socket");
+
+    // Build a socket
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port_num);
+    addr.sin_addr.s_addr = ip.s_addr;
+    memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
+
+    // Connect
+    res = connect(sockfd, (struct sockaddr *)(&addr), sizeof(addr));
+    if (res < 0)
+    {
+        close(sockfd);
+        handle_error("[-] Failed to connect");
+    }
+    printf("[+] Connected\n");
+
+    // Set fd_set struct
+    fd_set init_read_set, read_set;
+    FD_ZERO(&init_read_set);
+    FD_SET(0, &init_read_set);      // for stdin
+    FD_SET(sockfd, &init_read_set); // for socket
+
+    while (1)
+    {
+        read_set = init_read_set;
+
+        // Check
+        res = select(max_fd + 1, &read_set, NULL, NULL, NULL);
+        if (res < 0)
+        {
+            close(sockfd);
+            handle_error("[-] Fail to select");
+        }
+        else if (!res)
+        {
+            continue;
+        }
+        else
+        {
+            // If stdin ready
+            if (FD_ISSET(0, &read_set))
+            {
+                // Get a user's message
+                fgets(user_mes, BUF_SIZE, stdin);
+                // Send a user's message
+                ssize_t sent_len;
+                sent_len = send(sockfd, user_mes, strlen(user_mes) - 1, 0);
+                if (sent_len < 0)
+                {
+                    close(sockfd);
+                    handle_error("[-] Failed to send a user's message");
+                }
+            }
+            // If socket ready
+            if (FD_ISSET(sockfd, &read_set))
+            {
+                // Read echoed message
+                ssize_t received_len = recv(sockfd, echoed_mes, BUF_SIZE - 1, 0);
+                echoed_mes[received_len] = '\0';
+                if (received_len < 0)
+                {
+                    close(sockfd);
+                    handle_error("[-] Failed to recieve a echoed message");
+                }
+                printf("%s\n", echoed_mes);
+            }
+        }
+    }
+}
---
+++ select-server/select-server.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+/** Constants
+ * BUF_SIZE : TCP payload size
+*/
+#define BUF_SIZE 1 << 16
+
+/** Global variables
+ * char mes[BUF_SIZE] : buffer for message
+*/
+char mes[BUF_SIZE];
+
+void handle_error(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(EXIT_FAILURE);
+}
+
+void usage()
+{
+    printf("syntax : ./select-server <port number>\n");
+    printf("sample : ./select-server 8888\n");
+    handle_error("[-] Usage error");
+}
+
+int main(int argc, char *argv[])
+{
+    // Result value
+    int res;
+
+    // Check command-line argument
+    if (argc != 2)
+        usage();
+
+    // Parse command-line argument
+    int port_num = atoi(argv[1]);
+    if (port_num < 0 || (int)(1 << 16) <= port_num)
+        handle_error("[-] Invalid port number");
+
+    // For client
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int client_sockfd;
+
+    // Create a welcome socket
+    // IPv4 Internet protocols and TCP
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        handle_error("[-] Fail to create a socket");
+
+    // Build a socket address
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port_num);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
+
+    // Bind
+    res = bind(sockfd, (struct sockaddr *)(&addr), sizeof(struct sockaddr));
+    if (res < 0)
+    {
+        close(sockfd);
+        handle_error("[-] Fail to bind");
+    }
+
+    // Listen
+    res = listen(sockfd, SOMAXCONN);
+    if (res < 0)
+    {
+        close(sockfd);
+        handle_error("[-] Fail to listen");
+    }
+
+    // Set fd_set struct
+    fd_set init_read_set, read_set;
+    FD_ZERO(&init_read_set);
+    FD_SET(sockfd, &init_read_set); // for welcome socket
+    int max_fd = sockfd;
+    while (1)
+    {
+        read_set = init_read_set;
+
+        //소켓 포함 모든 파일디스크립터를 대상으로 '수신된 데이터의 존재여부' 검사
+        res = select(max_fd + 1, &read_set, NULL, NULL, NULL);
+        // If error,
+        if (res < 0)
+        {
+            perror("[-] Fail to select\n");
+            break;
+        }
+        // If timeout,
+        if (!res)
+        {
+            continue;
+        }
+        for (int i = 0; i < max_fd + 1; i++)
+        {
+            if (!FD_ISSET(i, &read_set))
+                continue;
+            // If connection request,
+            if (i == sockfd)
+            {
+                client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+                // Update init_read_set and max_fd
+                FD_SET(client_sockfd, &init_read_set);
+                max_fd = max_fd > client_sockfd ? max_fd : client_sockfd;
+                printf("[+] Client connected: %d\n", client_sockfd);
+            }
+            else
+            {
+                memset(mes, 0, sizeof(mes));
+                ssize_t received_len = recv(i, mes, BUF_SIZE - 1, 0);
+                mes[received_len] = '\0';
+                if (received_len < 0)
+                {
+                    fprintf(stderr, "[-] Fail to recieve a message from client(%d)\n", i);
+                    continue;
+                }
+                // Echo message
+                ssize_t sent_len;
+                sent_len = send(i, mes, strlen(mes), 0);
+                if (sent_len < 0)
+                {
+                    fprintf(stderr, "[-] Fail to echo a message to client(%d)\n", i);
+                    continue;
+                }
+                if (!sent_len)
+                {
+                    FD_CLR(i, &init_read_set);
+                    close(i);
+                    printf("[+] Closed client(%d)\n", i);
+                    continue;
+                }
+                printf("[+] Echo to client(%d) : %s\n", i, mes);
+            }
+        }
+    }
+    for (int i = 3; i < max_fd + 1; i++)
+    {
+        if (!FD_ISSET(i, &init_read_set))
+            continue;
+        close(i);
+    }
+    exit(EXIT_SUCCESS);
+}
